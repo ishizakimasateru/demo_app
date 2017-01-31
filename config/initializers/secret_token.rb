d10ba66cbd77c252d88b793fe48fb105b3406e0c
Masateru Ishizaki <ishizakimasateru@yahoo.co.jp>
2017-01-31 05:44:01 +0000

    Initialize depository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DemoApp::Application.config.secret_key_base = 'ff211c44866f31608ceed06b9810a7d447716179bfcd97f7f9b5e672b75e075a90d9694f6ea481670e7b91dff4f81124d5be639cdc13e9aeaa0812b4e33b824b'
